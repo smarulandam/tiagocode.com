24b79b9c5526fa7c813724b00c48fbd926dfe427
Santiago Marulanda <santiagomm1997@gmail.com>
2026-03-08 17:22:43 +0100

    chore: trigger website rebuild

--- apps/website/src/lib.rs
+++ apps/website/src/lib.rs
@@ -7,6 +7,7 @@ pub mod helpers;
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
+    // Client entrypoint used by cargo-leptos during hydration.
     use adapters::driver::leptos_webui::views::app::*;
     console_error_panic_hook::set_once();
     leptos::mount::hydrate_body(App);
